ee72c12e12629de1f8d67379f1e8e95295b491f9
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-10-04 19:33:26 +0100

    Reverse string additional test

--- reverseString.test.js
+++ reverseString.test.js
@@ -2,4 +2,5 @@ const reverseString = require("./reverseString");
 
 test("Properly reverses a string", () => {
   expect(reverseString("Mitchel")).toBe("lehctiM");
+  expect(reverseString("Mitchel Okonjo")).toBe("ojnokO lehctiM");
 });
